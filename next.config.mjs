dbb76f9cdade470a200e607c913f21000f0c83a7
Andy Nguyen <atnguyen@us.ibm.com>
2024-06-10 12:36:20 -0600

    test bath path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  assetPrefix: "./",
+  basePath: "",
   reactStrictMode: true,
 };
 
