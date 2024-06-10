d230a73d87a0875e970dca5c5f6c8754971103a7
Andy Nguyen <atnguyen@us.ibm.com>
2024-06-10 13:43:47 -0600

    base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: `${process.env.BASE_PATH || "/portfolio"}`,
+  basePath: `${process.env.BASE_PATH}`,
   reactStrictMode: true,
 };
 
