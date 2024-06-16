ebcaa5eaba45ecbb5b74d958b775af85ce106ce6
Andy Nguyen <atnguyen@us.ibm.com>
2024-06-16 07:53:27 -0600

    establishing the sectional structure of landing page

---
+++ README.md
@@ -0,0 +1,3 @@
+- parallax section
+--> https://codesandbox.io/p/sandbox/framer-motion-parallax-i9gwuc?file=%2Fsrc%2Fstyles.css%3A153%2C2&from-embed=
+- dark and light mode 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        primary: ["var(--font-bebas)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
