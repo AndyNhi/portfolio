7d02bd97ee5a12eeb0bfe43418dc37f75873f11c
Andy Nguyen <atnguyen@us.ibm.com>
2024-06-26 11:46:33 -0600

    wip, structure of logo framing

--- src/app/_components/Timeline.tsx
+++ src/app/_components/Timeline.tsx
@@ -1,3 +1,20 @@
+import me from "../../../public/me.jpg";
+import led2 from "../../../public/led2.jpg";
+import led3 from "../../../public/led3.jpg";
+import led4 from "../../../public/led4.jpg";
+import fss from "../../../public/fss.jpg";
+import travel from "../../../public/travel.jpg";
+import Image from "next/image";
+
 export default function Timeline() {
-  return <div className="h-full w-full text-white bg-red-500">timeline</div>;
+  return (
+    <div className="h-full w-full text-white ">
+      <div className="grid grid-cols-4 gap-0  h-full">
+        <div className="bg-red-400 opacity-40">dev ops logos</div>
+        <div className="bg-red-400 opacity-30">front end logos</div>
+        <div className="bg-red-400 opacity-20">back end logos</div>
+        <div className="bg-red-400 opacity-10">tools logos</div>
+      </div>
+    </div>
+  );
 }
